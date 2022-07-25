5c0a30c4971d51625a0a98a88c8244119315c6e2
SergioMate <sergiomatecastro@gmail.com>
2022-07-25 11:39:00 +0000

    fix: range

--- number_guess.sh
+++ number_guess.sh
@@ -2,7 +2,7 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-SECRET_NUMBER=$(($RANDOM % 10 + 1))
+SECRET_NUMBER=$(($RANDOM % 1000 + 1))
 
 echo "Enter your username:"
 read USERNAME
