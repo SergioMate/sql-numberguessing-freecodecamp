3ae3bf3a873e0212263fe60dcc34514a7c64b085
SergioMate <sergiomatecastro@gmail.com>
2022-07-25 10:52:32 +0000

    feat: greeting

--- number_guess.sh
+++ number_guess.sh
@@ -3,3 +3,17 @@
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 NUMBER=$(($RANDOM % 1000 + 1))
+
+echo "Enter your username:"
+read USERNAME
+PLAYER=$($PSQL "SELECT username, games_played, best_game FROM players WHERE username='$USERNAME'")
+if [ -z "$PLAYER" ]
+then
+  echo "Welcome, $USERNAME! It looks like this is your first time here"
+else
+  echo $PLAYER | {
+    IFS=" | "
+    read USERNAME GAMES_PLAYED BEST_GAME
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  }
+fi
